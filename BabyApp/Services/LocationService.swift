222fae02199c22ccb2d32b1fb1adaf24fea000c5
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:46 +0800

    Add BabyApp/Services/LocationService.swift

---
+++ BabyApp/Services/LocationService.swift
@@ -0,0 +1,40 @@
+import Foundation
+import CoreLocation
+
+protocol LocationService {
+    func requestPermission()
+    func currentLocation() async throws -> CLLocation
+}
+
+final class CoreLocationService: NSObject, LocationService, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    private var continuation: CheckedContinuation<CLLocation, Error>?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func requestPermission() {
+        manager.requestWhenInUseAuthorization()
+    }
+
+    func currentLocation() async throws -> CLLocation {
+        try await withCheckedThrowingContinuation { continuation in
+            self.continuation = continuation
+            self.manager.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            continuation?.resume(returning: location)
+            continuation = nil
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        continuation?.resume(throwing: error)
+        continuation = nil
+    }
+}
