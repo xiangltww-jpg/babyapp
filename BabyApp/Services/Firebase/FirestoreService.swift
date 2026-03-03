ba1409570c53c59943d285ff7467ebf753ee8627
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:45 +0800

    Add BabyApp/Services/Firebase/FirestoreService.swift

---
+++ BabyApp/Services/Firebase/FirestoreService.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+protocol FirestoreService {
+    func fetchBabies(familyId: String) async throws -> [Baby]
+    func createBaby(_ baby: Baby) async throws
+    func updateBaby(_ baby: Baby) async throws
+    func deleteBaby(babyId: String) async throws
+
+    func fetchEvents(babyId: String, from: Date, to: Date) async throws -> [Event]
+    func createEvent(_ event: Event) async throws
+    func updateEvent(_ event: Event) async throws
+    func deleteEvent(eventId: String) async throws
+
+    func fetchReminders(babyId: String) async throws -> [Reminder]
+    func upsertReminder(_ reminder: Reminder) async throws
+    func deleteReminder(reminderId: String) async throws
+}
+
+final class FirebaseFirestoreService: FirestoreService {
+    func fetchBabies(familyId: String) async throws -> [Baby] { [] }
+    func createBaby(_ baby: Baby) async throws {}
+    func updateBaby(_ baby: Baby) async throws {}
+    func deleteBaby(babyId: String) async throws {}
+
+    func fetchEvents(babyId: String, from: Date, to: Date) async throws -> [Event] { [] }
+    func createEvent(_ event: Event) async throws {}
+    func updateEvent(_ event: Event) async throws {}
+    func deleteEvent(eventId: String) async throws {}
+
+    func fetchReminders(babyId: String) async throws -> [Reminder] { [] }
+    func upsertReminder(_ reminder: Reminder) async throws {}
+    func deleteReminder(reminderId: String) async throws {}
+}
