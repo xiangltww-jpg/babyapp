9c5ebf23b85218a0cef6f881cfd38b68a70b21f1
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:55 +0800

    Add BabyApp/ViewModels/EventListViewModel.swift

---
+++ BabyApp/ViewModels/EventListViewModel.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+@MainActor
+final class EventListViewModel: ObservableObject {
+    @Published var events: [Event] = []
+    @Published var isLoading: Bool = false
+
+    private let firestore: FirestoreService
+
+    init(firestore: FirestoreService) {
+        self.firestore = firestore
+    }
+
+    func loadEvents(babyId: String, from: Date, to: Date) async {
+        isLoading = true
+        defer { isLoading = false }
+        do {
+            events = try await firestore.fetchEvents(babyId: babyId, from: from, to: to)
+        } catch {
+            events = []
+        }
+    }
+}
