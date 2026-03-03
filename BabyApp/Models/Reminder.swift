c79ef7e4468f84996f3948defe2bed3f8d480e7b
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:39 +0800

    Add BabyApp/Models/Reminder.swift

---
+++ BabyApp/Models/Reminder.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+enum ReminderMode: String, Codable, CaseIterable {
+    case fixedTime
+    case habitBased
+    case custom
+
+    var titleCN: String {
+        switch self {
+        case .fixedTime: return "固定时间"
+        case .habitBased: return "习惯提醒"
+        case .custom: return "自定义"
+        }
+    }
+}
+
+struct Reminder: Identifiable, Codable, Equatable {
+    let id: String
+    var babyId: String
+    var type: EventType
+    var mode: ReminderMode
+    var time: Date?
+    var intervalMinutes: Int?
+    var enabled: Bool
+    var createdAt: Date
+}
