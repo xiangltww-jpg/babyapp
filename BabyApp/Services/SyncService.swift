3d16e0860cd7dfd48cc3c4ca75cfb065b72d4477
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:51 +0800

    Add BabyApp/Services/SyncService.swift

---
+++ BabyApp/Services/SyncService.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+protocol SyncService {
+    func startSync(for babyId: String)
+    func stopSync()
+}
+
+final class DefaultSyncService: SyncService {
+    func startSync(for babyId: String) {
+        // TODO: start listener for events/reminders
+    }
+
+    func stopSync() {
+        // TODO: stop listeners
+    }
+}
