7538094abe515d8efeb031118165265b8c457e20
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:59:12 +0800

    Add BabyApp/Views/SettingsView.swift

---
+++ BabyApp/Views/SettingsView.swift
@@ -0,0 +1,25 @@
+﻿import SwiftUI
+
+struct SettingsView: View {
+    @EnvironmentObject var appState: AppState
+
+    var body: some View {
+        NavigationStack {
+            List {
+                Section("账户") {
+                    Text(appState.currentUser?.email ?? "未登录")
+                    Button("退出登录") {
+                        Task { try? await appState.environment.authService.signOut() }
+                    }
+                }
+
+                Section("宝宝") {
+                    Button("管理宝宝") {
+                        // TODO: open baby management
+                    }
+                }
+            }
+            .navigationTitle("设置")
+        }
+    }
+}
