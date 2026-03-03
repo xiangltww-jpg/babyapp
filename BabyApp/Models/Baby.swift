87304130640c62ecb5e3de84c23da2076bf9a06d
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:36 +0800

    Add BabyApp/Models/Baby.swift

---
+++ BabyApp/Models/Baby.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Baby: Identifiable, Codable, Equatable {
+    let id: String
+    var familyId: String
+    var name: String
+    var birthDate: Date
+    var gender: String?
+    var createdAt: Date
+}
