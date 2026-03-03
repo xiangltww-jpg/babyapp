e7ec85150dbb5eb334593ccad967341997c1f501
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:59:00 +0800

    Add BabyApp/Views/BabySwitchView.swift

---
+++ BabyApp/Views/BabySwitchView.swift
@@ -0,0 +1,23 @@
+﻿import SwiftUI
+
+struct BabySwitchView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("当前宝宝")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                Text("小熊")
+                    .font(.title3.bold())
+            }
+            Spacer()
+            Button("切换") {
+                // TODO: open baby selector
+            }
+            .buttonStyle(.bordered)
+        }
+        .padding()
+        .background(.thinMaterial)
+        .cornerRadius(12)
+    }
+}
