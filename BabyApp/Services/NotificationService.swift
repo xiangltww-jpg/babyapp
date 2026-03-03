cadac6eb7ff5519215d06d64700398b4d4f06886
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:48 +0800

    Add BabyApp/Services/NotificationService.swift

---
+++ BabyApp/Services/NotificationService.swift
@@ -0,0 +1,23 @@
+import Foundation
+import UserNotifications
+
+protocol NotificationService {
+    func requestPermission() async throws
+    func scheduleReminder(_ reminder: Reminder) async throws
+    func cancelReminder(reminderId: String) async
+}
+
+final class UserNotificationService: NotificationService {
+    func requestPermission() async throws {
+        let center = UNUserNotificationCenter.current()
+        _ = try await center.requestAuthorization(options: [.alert, .sound, .badge])
+    }
+
+    func scheduleReminder(_ reminder: Reminder) async throws {
+        // TODO: map Reminder to UNNotificationRequest
+    }
+
+    func cancelReminder(reminderId: String) async {
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [reminderId])
+    }
+}
