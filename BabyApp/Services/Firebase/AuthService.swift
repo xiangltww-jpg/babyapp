84ecd08034b18d70828291048350ed29106aaeb4
xiangltww-jpg <xiangltww@gmail.com>
2026-03-03 22:58:42 +0800

    Add BabyApp/Services/Firebase/AuthService.swift

---
+++ BabyApp/Services/Firebase/AuthService.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Combine
+
+protocol AuthService {
+    var authStatePublisher: AnyPublisher<UserProfile?, Never> { get }
+    func signIn(email: String, password: String) async throws
+    func signUp(email: String, password: String) async throws
+    func signOut() async throws
+}
+
+final class FirebaseAuthService: AuthService {
+    private let subject = CurrentValueSubject<UserProfile?, Never>(nil)
+
+    var authStatePublisher: AnyPublisher<UserProfile?, Never> {
+        subject.eraseToAnyPublisher()
+    }
+
+    func signIn(email: String, password: String) async throws {
+        // TODO: Firebase Auth sign-in
+        subject.send(UserProfile(id: UUID().uuidString, email: email, displayName: nil, createdAt: Date()))
+    }
+
+    func signUp(email: String, password: String) async throws {
+        // TODO: Firebase Auth sign-up
+        subject.send(UserProfile(id: UUID().uuidString, email: email, displayName: nil, createdAt: Date()))
+    }
+
+    func signOut() async throws {
+        // TODO: Firebase Auth sign-out
+        subject.send(nil)
+    }
+}
